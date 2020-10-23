1ac9a938fa4f580cb62cc8b8a63141b77c8d65d7
JyotirmayNirankari <72866018+JyotirmayNirankari@users.noreply.github.com>
2020-10-23 17:19:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bullet;
+var wall,thickness;
+var speed;
+var weight;
+var deformation=0;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+bullet=createSprite(50,200,50,50);
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=(80,80,80);
+  bullet.velocityX=speed;
+  deformation=(0.5*weight*speed*speed)/22500;
+}
+
+function draw() {
+  background(255,255,255);  
+if(hasCollided(bullet,wall)){
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  if(damage>0){
+    wall.shapeColor=color(255,0,0);
+  }
+  if(damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+}
+
+  drawSprites();
+}
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
+}
